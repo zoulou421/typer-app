97469c98b818f6f4a12887136a7ff2f1f6ac43e8
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-21 15:24:32 +0200

    added: option

--- my_app.py
+++ my_app.py
@@ -1,11 +1,13 @@
 import typer
 
 
-def my_app(extension: str = typer.Argument("txt",help="extension to search")):
+def my_app(delete: bool = typer.Option(..., help="Delete files found"),
+           extension: str = typer.Argument("txt", help="extension to search")):
     """
      Show Files with  data extension file
     """
     typer.echo(f"Re-search file with extension {extension}")
+    print(delete)
 
 
 if __name__ == '__main__':
