10a946f11bcb8da6188c9102a3fa89ac924f86b6
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-21 14:45:50 +0200

    added: typer basis app

---
+++ my_app.py
@@ -0,0 +1,12 @@
+import typer
+
+
+def my_app():
+    """
+     File with extension file
+    """
+    typer.echo("Re-search file with extension")
+
+
+if __name__ == '__main__':
+    typer.run(my_app)
