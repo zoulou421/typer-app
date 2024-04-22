2f187b49dc9852fadd376a17629b8e9ced042b9a
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-22 17:44:35 +0200

    added: readme file and pic

---
+++ README.md
@@ -0,0 +1,33 @@
+#Leanr to BUILD a Command Line App with Typer 
+
+Note: Files in this  project are enough documented.
+Every file almost represent a small project.
+So you can pick one by one and try to understand the source code.
+
+
+How to USE Typer as a db.
+
+How to CREATE your own commands.
+ 
+How to COLORATE Or put STYLE your SYNTAXE.
+
+
+## Documentation
+
+[Documentation](https://pypi.org/project/typer/)
+
+![1.JPG](1.JPG)
+
+![cwd.JPG](cwd.JPG)
+
+![Capture.JPG](Capture.JPG)
+
+## 🚀 About Author
+Bonevy BEBY is a Software Engineer,an Accross Plateforms Archictecturer and a Trainer. He's also passionnate with DevOPs and Cloud Services.
+
+
+## 🛠 Skills
+Javascript, HTML, CSS,java,Javascript,Typescript,Python,PHP,DOT.NET...
+
+@Accross plateforms trainer
+My contacts: 00221 77 862 72 72 | bonevybeby@gmail.com
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import typer
 from typing import Optional
 
+from pathlib import Path
+
 
 def main(extension: str,
          directory: Optional[str] = typer.Argument(None, help="file in which you have to search"),
@@ -8,7 +10,26 @@ def main(extension: str,
     """
     Displays files found with given extension.
     """
-    print(extension, directory, delete)
+    # print(extension, directory, delete)
+    if not directory:
+        directory = Path(directory)
+    else:
+        directory = Path.cwd()  # current working directory
+    if not directory.exists():
+        typer.secho(f"Directory {directory} not exits", fg=typer.colors.RED)
+        raise typer.Exit()
+
+    files = directory.rglob(f"*.{extension}")
+    if delete:
+        typer.confirm("Do you want to delete all files found ?",abort=True)
+        for file in files:
+            file.unlink()
+            typer.echo(f"Deletion of file {file}",fg=typer.colors.RED)
+    else:
+        typer.echo(f"Files found extension: {extension}", fg=typer.colors.BLUE)
+        for file in files:
+            typer.echo(file)
+
 
 
 if __name__ == "__main__":
